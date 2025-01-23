55614692a99e28537ce7b2e77ae880c07de05919
Aayush Dwivedi <128339505+Ayush20002@users.noreply.github.com>
2025-01-23 20:13:33 +0530

    Update middleware.js

--- ai/middleware.js
+++ ai/middleware.js
@@ -1,14 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
+ 
 
-const isProtectedRoute = createRouteMatcher([
-
-
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in", "/sign-up"],
+  
 });
-
+ 
 export const config = {
-  matcher: ["/((?!.*\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
